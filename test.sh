97e6603b34837fb127e7e13cf1c81128b56557cb
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2023-10-19 13:58:49 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!bin/bash
 
 touch test.txt
-echo "${DB_USERNAME}" >> "test.txt"
+echo " Username : ${DB_USERNAME}" >> "test.txt"
+echo "Hostname: ${DB_HOSTNAME}" >> "test.txt"
+echo "cred_id: ${cred_id}" >> "test.txt"
